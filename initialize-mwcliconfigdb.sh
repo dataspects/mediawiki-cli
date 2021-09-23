8004200bf5857c421b254b4470006373e3f08224
Lex Sulzer <lex@dataspects.com>
2021-09-23 13:53:59 +0200

    export

--- initialize-mwcliconfigdb.sh
+++ initialize-mwcliconfigdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Public MWMBashScript:
-
+echo $MWCLI_CONFIG_DB_ON_HOSTING_SYSTEM
 sqlite3 $MWCLI_CONFIG_DB_ON_HOSTING_SYSTEM "CREATE TABLE IF NOT EXISTS extensions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name text,
